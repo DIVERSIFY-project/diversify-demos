e7fc46b380b7a6802d2fb380b46c66d5294c6fdd
Erwan Daubert <erwan.daubert@gmail.com>
2014-03-13 00:24:35 +0100

    clean modelGenerator

---
+++ sosies-demo/org.diversify.kevoree.generator/src/main/java/org/diversify/kevoree/generator/ModelGenerator.java
@@ -0,0 +1,16 @@
+package org.diversify.kevoree.generator;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 12/03/14
+ * Time: 17:43
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+public class ModelGenerator {
+
+    public static void main(String[] args) {
+        new ModelGeneratorFromModel().main(args);
+    }
+}
