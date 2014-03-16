4e9ff4f28a39bd6314ee69bc95fd2ff0542f712e
Erwan Daubert <erwan.daubert@gmail.com>
2014-03-16 22:44:07 +0100

    filter backend server

--- sosies-demo/org.diversify.kevoree.loadBalancer/src/main/java/org/diversify/kevoree/loadBalancer/KevoreeLBMonitor.java
+++ sosies-demo/org.diversify.kevoree.loadBalancer/src/main/java/org/diversify/kevoree/loadBalancer/KevoreeLBMonitor.java
@@ -142,7 +142,7 @@ public class KevoreeLBMonitor extends ModelListenerAdapter {
                             if (content.split(";").length >= 2 && !content.split(";")[2].trim().equals("-")) {
                                 String[] hosts = content.split(";")[2].trim().split(",");
                                 for (String host : hosts) {
-                                    if (!host.startsWith("127.0.0")) {
+                                    if (!host.startsWith("127.0.0") && !host.startsWith("backend")) {
                                     String toSend = content.replace(content.split(";")[2], host);
                                     notifyRequest.send(toSend);
                                     server.sendToAll(toSend);
